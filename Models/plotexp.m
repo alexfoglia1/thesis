fac528bd9067561b79c2313d7ebc3f153b2901a3
Alex Foglia <alex.f1793@gmail.com>
2019-10-10 19:39:59 +0200

    ok

--- Models/plotexp.m
+++ Models/plotexp.m
@@ -26,20 +26,21 @@ exp13velX = [0.0184494 , 0.129497 , 0.0222426 ];
 exp13velY = [0.0396467 , 0.863711 , 0.084737 ];
 exp13velZ = [0.00355928 , 0.0189619 , 0.00306268 ];
 
-exp21ecefX = [4.3248, 5.41018, 20.5617];
-exp21ecefY = [0.0226056, 0.04816, 0.39742];
-exp21ecefZ = [3.81041e-06, 9.04865e-06, 3.30294e-05];
-exp21velX = [0.0248871, 0.0260141, 0.150687];
-exp21velY = [0.0475246, 0.0899591, 0.908185];
-exp21velZ = [0.00406042 0.00340827 0.0228906];
+ecefXToPlot = [ exp11ecefX(3), exp12ecefX(3), exp13ecefX(3) ];
+ecefYToPlot = [ exp11ecefY(3), exp12ecefY(3), exp13ecefY(3) ];
+ecefZToPlot = [ exp11ecefZ(3), exp12ecefZ(3), exp13ecefZ(3) ];
+ecefXGoldToPlot = [ ecefXgold(3), ecefXgold(3), ecefXgold(3) ];
+ecefYGoldToPlot = [ ecefXgold(3), ecefXgold(3), ecefXgold(3) ];
+ecefZGoldToPlot = [ ecefXgold(3), ecefXgold(3), ecefXgold(3) ];
 
-exp22ecefX = [562.483 318.16 1009.02];
-exp22ecefY = [148.597 203.971 811.198];
-exp22ecefZ = [0.0377065 0.0298226 0.116483];
-exp22velX = [19.8981 29.438 114.018];
-exp22velY = [89.8 156.398 613.732];
-exp22velZ = [18.5734 38.0946 163.815];
-semilogy(ecefXgold, '*-');
-hold;
-semilogy(exp12ecefX, '*-');
-legend('Golden Run Ecef X (mean, sigma, max)','Experimental Ecef X (mean, sigma, max)')
+semilogy(ecefXToPlot);
+hold on;
+semilogy(ecefYToPlot);
+semilogy(ecefZToPlot);
+semilogy(ecefXGoldToPlot);
+semilogy(ecefYGoldToPlot);
+semilogy(ecefZGoldToPlot);
+
+velXToPlot = [ exp11ecefX(3), exp12ecefX(3), exp13ecefX(3) ];
+velYToPlot = [ exp11ecefX(3), exp12ecefX(3), exp13ecefX(3) ];
+velZToPlot = [ exp11ecefX(3), exp12ecefX(3), exp13ecefX(3) ];
